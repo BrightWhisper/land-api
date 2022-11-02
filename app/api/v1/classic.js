0df744532dd7f50ef77b87d608cd3de970ac1880
wangshuyu01 <wangshuyu01@58.com>
2022-11-02 19:57:55 +0800

    添加加密模块

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -8,10 +8,6 @@ const {
 } = require("../../../core/http-exception")
 
 router.post("/v1/:id/classic/latest", async(ctx, next) => {
-  // const path = ctx.params
-  // const query = ctx.request.query
-  // const header = ctx.request.header
-  // const body = ctx.request.body
   const v = await new PositiveIntergerValidator().validate(ctx) 
   const id = v.get('body.key', false)
 
--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs")
+
 const Router = require("koa-router")
 const router = new Router({
   prefix: "/v1/user",
@@ -7,11 +9,17 @@ const { User } = require("../../models/user")
 
 router.post("/register", async (ctx, next) => {
   const v = await new RegisterValidator().validate(ctx)
+  // 密码不能以明文来存储
+  // 加密需要一个盐,参数指的是计算机生成盐的成本，一般是10,当然成本越高，安全性也就越高
+  // 同一密码用此方法加密后生成的字符串是不同的,这一点用于防御彩虹攻击
+  const salt = bcrypt.genSaltSync(10)
+  const psw = bcrypt.hashSync(v.get("body.password2"), salt)
   const user = {
     email: v.get("body.email"),
-    password: v.get("body.password2"),
+    password: psw,
     nickname: v.get("body.nickname"),
   }
+  // 插入一条记录
   const r = await User.create(user)
   const id = v.get("body.key", false)
 
